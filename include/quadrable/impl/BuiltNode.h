2607d4ee5c4c2d47af089c09f78d2364174e1aa9
carter <carter>
2023-02-04 20:17:07 +0800

    update

--- include/quadrable/impl/BuiltNode.h
+++ include/quadrable/impl/BuiltNode.h
@@ -1,5 +1,5 @@
 
-constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+static constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
                            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 static std::string buffer_to_hex_string_2(const uint8_t * data, int len)
